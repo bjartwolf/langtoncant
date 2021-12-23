a86bb05748bdc866fad7c41e2517ef9a5801e2b6
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2021-12-23 21:54:07 +0100

    Fix with new cc 65 debugger setup

--- mk.bat
+++ mk.bat
@@ -3,7 +3,6 @@ set SCRIPT_DIR=%CD%
 popd
 
 for /D %%I in ("%USERPROFILE%\.vscode\extensions\entan-gl.cc65-vice-*") do @set EXTENSION_PATH=%%~I\build.bat
-
 if exist "%SCRIPT_DIR%\..\..\..\build.bat" (
     "%SCRIPT_DIR%\..\..\..\build.bat" make.exe %*
 ) else if exist "%EXTENSION_PATH%" (
